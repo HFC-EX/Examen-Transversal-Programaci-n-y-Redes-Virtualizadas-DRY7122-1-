5f008730cfd9ed111de6ce2dc2ea3d0f867a57e2
NetAcad DEVASC <changeme@example.com>
2023-07-09 02:41:20 +0000

    ITEM1-ET

---
+++ Item1.py
@@ -0,0 +1,9 @@
+print("---------------------------")
+print("     Integrantes\n")
+print("Hernan Ferreira Painefilo")
+print("Felipe Faundez Cornejo")
+print("Cristopher Tobar Tobar")
+print("---------------------------")
+
+
+
